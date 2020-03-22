53d2e374deeb66af1514cb63b35af1a83b70f043
realAXL <github2011@realaxl.de>
2020-03-22 16:16:55 +0100

    REST load + Maps

---
+++ ui_design/bootstrap_quickNdirty/res/js/data.js
@@ -0,0 +1,21 @@
+// data.js
+
+function getRealtimeData(piHostname) {
+	renderSpinner()
+	$.get("https://dweet.io:443/get/latest/dweet/for/" + piHostname, function(data, status) {
+		// alert("Data: " + JSON.stringify(data) + "\nStatus: " + status);
+		renderLocationCard(data)
+	})
+}
+
+
+function prepGoogleMaps(data) {
+	let loc = [data.street, data.postal, data.city].join(' ')
+	loc = loc.replace(' ', '%20')
+
+
+	let link = 'https://maps.google.com/maps?q=' + loc + '&ie=UTF8&iwloc=&output=embed'
+	let content = '<iframe height="600" id="gmap_canvas" class="z-depth-1-half map-container-5" frameborder="0" style="border:0" allowfullscreen src="' + link + '"></iframe>'
+
+	return content
+}
--- ui_design/bootstrap_quickNdirty/res/js/render.js
+++ ui_design/bootstrap_quickNdirty/res/js/render.js
@@ -22,35 +22,86 @@ let fakeData = {
 }
 
 
+
+const fakeLocationData = {
+	'pi-box-02': {
+		label: 'Aldi Berlin-Kreuzberg',
+		street: 'Oranienstraße 140-142',
+		postal: '10969',
+		city: 'Berlin',
+		geoCoords: { lat: -25.344, lng: 131.036 }
+	},
+	'pi-box-04': {
+		label: 'ALDI Berlin-Moabit',
+		street: 'Huttenstraße 41 - 44',
+		postal: '10553',
+		city: 'Berlin',
+		geoCoords: { lat: -25.344, lng: 131.036 }
+	}
+}
+
+
+function renderSpinner(data) {
+	let header = div('card-header', 'Lade Daten ...')
+	let body = div('card-body text-center', '<img src="res/gfx/loading.gif" alt="Spinner"/>')
+	let footer = div('card-footer text-right text-muted', '...')
+
+	let card = div('card', header + body + footer)
+
+	document.getElementById('cardTest').innerHTML = card
+}
+
 function renderLocationCard(data) {
 	// document.getElementById('preformatted').innerHTML = JSON.stringify(data, null, '\t');
 
 
 	let pos = data.with[0]
+	let id = pos.thing
 
 	let lastUpdate = timeConverter(pos.content.unixtime)
 
 	let pin = div("col-sm-2", '<img src="res/gfx/positionsstift.svg" width="40px">')
-	let location = div("col-sm-6", pos.thing)
+	let location = div("col-sm-6", renderLocationBody(id))
 
-	let crowd = '<h2 style="display: block;">' + pos.content.anzap + '</h2>'
-    crowd += '<img src="res/gfx/Bimetrical_icon_person_black.svg">'.repeat(Math.min(7, pos.content.anzap))
-    
+	let crowd = '<h1 class="display-3" style="display: block;">' + pos.content.anzap + '</h1>'
+	crowd += '<img src="res/gfx/Bimetrical_icon_person_black.svg">'.repeat(Math.min(7, pos.content.anzap))
+
 	let persons = div("col-sm-4", crowd)
 
 	let bodyRow = div('row', pin + location + persons)
 
 	let header = div('card-header', pos.thing)
 	let body = div('card-body', bodyRow)
-	let footer = div('card-footer text-right text-muted', '<small>Aktualisierung; <b>' + lastUpdate + '</b></small>')
+	let footer = div('card-footer text-right text-muted', '<small>Aktualisierung: <b>' + lastUpdate + '</b> (' + pos.thing + ')</small>')
 
 	let card = div('card', header + body + footer)
 
 	document.getElementById('cardTest').innerHTML = card
 
+	if (id in fakeLocationData) {
+		document.getElementById('mapHolder').innerHTML = prepGoogleMaps(fakeLocationData[id])
+	} else {
+		document.getElementById('mapHolder').innerHTML = "Standort nicht gefunden: " + id
+	}
 
 	console.log(timeConverter(data.with[0].content.unixtime));
 	console.log(div('testClass', 'Content'))
+}
+
+
+
+function renderLocationBody(id) {
+	if (id in fakeLocationData) {
+		let loc = fakeLocationData[id]
+		let result = '<h4>' + loc.label + '</h4>'
+		result += '<p>' + loc.street + '<br/>'
+		result += loc.postal + ' ' + loc.city + '</p>'
+		result += '<p class="text-muted text-right">' + id + '<p/>'
+
+		return result
+	} else {
+		return div('text-danger', 'Standort nicht gefunden: ' + id)
+	}
 }
 
 
